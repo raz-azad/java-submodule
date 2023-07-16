8e8f76cb365c3188420f0bb75bcb3e395586b3ba
Rashed Azad <rashed.azad@gmail.com>
2023-07-16 05:33:04 +0200

    updated after ClassAndInterface finished

---
+++ BinarySearch/src/BinaryCalculation.java
@@ -0,0 +1,2 @@
+public class BinaryCalculation {
+}
---
+++ BinarySearch/src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        byte size8Bit = 10;
+        short size16Bit = 1000;
+        int size32Bit = 1000;
+        float size32BitFloat = 1000.5f;
+        double size64BitDouble = 1000.5d;
+        char size8BitChar = 'C';
+    }
+}
